c7457048a3edfc0cd964c3a0a87f1da65ee40391
Jack Halford <jack@crans.org>
2017-10-08 21:30:28 +0200

    libft hard copy

---
+++ libft/srcs/net/reserve_port.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   reserve_port.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jhalford <jack@crans.org>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/10/07 18:02:55 by jhalford          #+#    #+#             */
+/*   Updated: 2017/10/08 21:21:55 by jhalford         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "net.h"
+
+int	reserve_port(int s, struct sockaddr *sa)
+{
+	unsigned short		i;
+
+	i = 49152;
+	while (i < 65535)
+	{
+		sa->sin_port = htons(i);
+		if (bind(s, sa, sizeof(sa)) == 0)
+			return (0);
+		++i;
+	}
+	return (1);
+}
